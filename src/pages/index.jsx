8ccd03b2960517bef4d21058184e58f775141538
Josh Karnofsky <jkarno@seas.upenn.edu>
2015-09-05 19:19:27 -0400

    pulls statuses

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,18 +14,35 @@ var HomePage = React.createClass({
       layout: DefaultLayout
     };
   },
+  getInitialState() {
+    return {posts: []};
+  },
   render() {
+    var listItems = this.state.posts.map(function(item) {
+      return <li key={item.id}>{item.message}</li>;
+    });
     return (
       <div className="facebookButton">
         <button type="button" onClick={this.handleFacebookClick}>Pull Facebook Data</button>
+        <button type="button" onClick={this.handleStatusClick}>Log Statuses</button>
+        <ul>{listItems}</ul>
       </div>
     );
   },
   handleFacebookClick(e) {
-    FB.api(
-      "/me",
-      function (response) {
-        console.log(response);
+    FB.login(function(response){
+      window.localStorage.setItem("accessToken", response.authResponse.accessToken);
+      window.localStorage.setItem("userID", response.authResponse.userID);
+    }, {scope: 'public_profile,user_status,user_posts,read_page_mailboxes'});
+  },
+  handleStatusClick(e) {
+    var reactClass = this;
+    var token = window.localStorage.getItem("accessToken");
+    FB.api('/me/posts?limit=999',
+      {accessToken: token},
+      function(response) {
+        reactClass.state.posts = response.data;
+        reactClass.forceUpdate();
       }
     );
   }
